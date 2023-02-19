4a539c4665fe2ffbce63cbad89db15755916573f
AmineBechar <aminebechar96@gmail.com>
2023-02-19 09:22:41 +0100

    checkout

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', [DashboardController::class,'index'])->name('dashboardIndex');
-    //Route::resource('dashboard', DashboardController::class);
+    //Route::resource('dashboard', DashboardController::cla);
 
     Route::post('/update_display', [CategoryController::class,'update_display']);
     Route::resource('categories', CategoryController::class);
